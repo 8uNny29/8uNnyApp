2237b616329915d00a19896e2e8c282faad39f61
Muhammad Rafi Rabani <86810901+8uNny29@users.noreply.github.com>
2025-02-23 03:49:47 +0700

    V2.1.1

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,16 +1,17 @@
 const mysql = require("mysql");
+require("dotenv").config(); // Memuat file .env
 
 const dbconfig = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "8unnyapps",
-  multipleStatements: true,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: process.env.DB_MULTIPLE_STATEMENTS,
 });
 
 dbconfig.connect(function (err) {
   if (err) throw err;
-  console.log("Database Berhasil terkoneksi dengan baik!");
+  console.log("Database berhasil terkoneksi dengan baik!");
 });
 
 module.exports = dbconfig;
--- sql/databasev1.sql
+++ sql/databasev1.sql
@@ -16,24 +16,42 @@
 
 
 -- Dumping database structure for 8unnyapps
-CREATE DATABASE IF NOT EXISTS `8unnyapps` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
-USE `8unnyapps`;
+-- CREATE DATABASE IF NOT EXISTS `8unnyapps` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+-- USE `8unnyapps`;
 
 -- Dumping structure for table 8unnyapps.accounts
 CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` int(250) NOT NULL,
-  `username` varchar(50) NOT NULL DEFAULT '',
-  `password` varchar(50) NOT NULL DEFAULT '',
-  `kelas` varchar(50) NOT NULL DEFAULT '',
-  `divisi` varchar(50) NOT NULL DEFAULT '',
-  `email` varchar(50) NOT NULL DEFAULT '',
-  `no_telpon` int(25) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `id` int(250) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `nama` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `kelas` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `divisi` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `no_telpon` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `pengurus` enum('Y','N') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`nama`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table 8unnyapps.accounts: ~1 rows (approximately)
-INSERT INTO `accounts` (`id`, `username`, `password`, `kelas`, `divisi`, `email`, `no_telpon`) VALUES
-	(0, 'admin', 'admin', '', '', '', 0);
+-- Dumping data for table 8unnyapps.accounts: ~2 rows (approximately)
+INSERT INTO `accounts` (`id`, `username`, `password`, `nama`, `kelas`, `divisi`, `email`, `no_telpon`, `pengurus`) VALUES
+	(1, 'admin', '$2b$10$BuENVHOBBAhCV676ubt3E.K6KgG79SrDtHGXUohqZgFYMx9JytnS6', 'Admin', 'XI TKJ 3', 'Penginstalan', 'muhammad.rafi77105@smk.belajar.id', '085860699622', 'Y'),
+	(10, 'oomyyn', '$2b$10$lD0JCyJWx/U4LxohrGUlwuO41.1Oss1M.iAMPar3KTB7YMrD9Uex.', 'Mayza Rosmayanti', 'XI TKJ 3', 'Desain', 'mayza.rosmayanti23@smk.belajar.id', '083822740990', 'Y');
+
+-- Dumping structure for table 8unnyapps.pendaftaran
+CREATE TABLE IF NOT EXISTS `pendaftaran` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `kelas` varchar(50) DEFAULT NULL,
+  `divisi` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table 8unnyapps.pendaftaran: ~0 rows (approximately)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
